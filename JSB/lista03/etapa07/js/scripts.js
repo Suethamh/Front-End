6b525faf9c2e1dfb89ac273c2bd8622bbc31bff0
Matheus Henrique <163579270+Suethamh@users.noreply.github.com>
2024-10-12 12:47:15 -0300

    Update scripts.js

--- JSB/lista03/etapa07/js/scripts.js
+++ JSB/lista03/etapa07/js/scripts.js
@@ -1,3 +1,3 @@
 var dado = parseInt((Math.random() * 6) + 1);
 
-document.write(`<img src="/img/dado${dado}.png" alt="dado${dado}">`)
+document.write(`<img src="../img/dado${dado}.png" alt="dado${dado}">`)
